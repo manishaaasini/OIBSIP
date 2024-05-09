88c87cefe6bebceccf18196a9a5c15d4d20dc3dc
manishaaasini <117998650+manishaaasini@users.noreply.github.com>
2024-05-09 20:14:25 +0530

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,13 +1,8 @@
-# Python program to generate random
-# password using Tkinter module
 import random
 import string
 import pyperclip
 from tkinter import *
 from tkinter.ttk import *
-
-# Function for calculation of password
-
 
 def low(exclude_chars):
     entry.delete(0, END)
@@ -42,59 +37,48 @@ def low(exclude_chars):
 
 
 
-# Function for generation of password
+
 def generate():
     exclude_chars = exclude_entry.get().strip()
     password1 = low(exclude_chars)
     entry.insert(10, password1)
 
 
-# Function for copying password to clipboard
+
 def copy1():
     random_password = entry.get()
     pyperclip.copy(random_password)
 
 
-# Main Function
-
-# create GUI window
 root = Tk()
 var = IntVar()
 var1 = IntVar()
 
-# Title of your GUI window
+
 root.title("Random Password Generator")
 
-# create label and entry to show
-# password generated
 Random_password = Label(root, text="Password")
 Random_password.grid(row=0)
 entry = Entry(root)
 entry.grid(row=0, column=1)
 
-# create label for length of password
+
 c_label = Label(root, text="Length")
 c_label.grid(row=1)
 
-# create label for excluded characters
 exclude_label = Label(root, text="Exclude Characters")
 exclude_label.grid(row=2)
 
-# create entry for excluded characters
+
 exclude_entry = Entry(root)
 exclude_entry.grid(row=2, column=1)
 
-# create Buttons Copy which will copy
-# password to clipboard and Generate
-# which will generate the password
 copy_button = Button(root, text="Copy", command=copy1)
 copy_button.grid(row=0, column=2)
 generate_button = Button(root, text="Generate", command=generate)
 generate_button.grid(row=0, column=3)
 
-# Radio Buttons for deciding the
-# strength of password
-# Default strength is Medium
+
 radio_low = Radiobutton(root, text="Low", variable=var, value=1)
 radio_low.grid(row=1, column=2, sticky='E')
 radio_middle = Radiobutton(root, text="Medium", variable=var, value=0)
@@ -103,7 +87,7 @@ radio_strong = Radiobutton(root, text="Strong", variable=var, value=3)
 radio_strong.grid(row=1, column=4, sticky='E')
 combo = Combobox(root, textvariable=var1)
 
-# Combo Box for length of your password
+
 combo['values'] = (8, 9, 10, 11, 12, 13, 14, 15, 16,
                    17, 18, 19, 20, 21, 22, 23, 24, 25,
                    26, 27, 28, 29, 30, 31, 32, "Length")
@@ -111,5 +95,4 @@ combo.current(0)
 combo.bind('<<ComboboxSelected>>')
 combo.grid(column=1, row=1)
 
-# start the GUI
 root.mainloop()
